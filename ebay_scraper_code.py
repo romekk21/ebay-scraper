5c5ae309cb3b375dc4a1dbe433ca4d92da8e3c62
romekk21 <romek.kozdrowicz@gmail.com>
2022-08-09 18:24:29 -0400

    RunTime Update
    Removed previous double for loop in the code resulting in a more efficient runtime. Program now pulls the data from excel and overwrites it.

--- ebay_scraper_code.py
+++ ebay_scraper_code.py
@@ -60,7 +60,7 @@ def parse_ebay():
     title_data = dict()
 
     # Clean the data
-    stopwords = ["/", "to", "~","a", "Men's", "men's", "&", "mens", "men", "tee", "vintage", "vtg", "on", "of", "the", "t-shirt", "shirt", "shirt,", "sz", "size", "single", "-", "all", "in", "t"]
+    stopwords = ["i","ebay","shop","women","for", "/", "to", "~","a", "men’s", "men's", "&", "and","tshirt","mens", "men", "tee", "vintage", "vtg", "on", "of", "the", "t-shirt", "shirt", "shirt,", "sz", "size", "single", "-", "all", "in", "t"]
 
     # For each FULL title 
     for title in titles:
@@ -75,41 +75,32 @@ def parse_ebay():
                 continue
 
             # Important that items titled "M" are not separate from items titled "Medium"; it's the same size
-            # M = Medium, L = Large, XXL = 2XL
+            # M = Medium, L = Large, XXL = 2XL, S = Small
             if word.lower() == "m":
                 word = "Medium"
             elif word.lower() == "l":
                 word = "Large"
             elif word.lower() == "xxl":
                 word = "2XL"
-            elif word.lowe() == "s":
+            elif word.lower() == "s":
                 word = "Small"
 
             # Add to dictionary, keep track of how many of the same words are found
-            if word in title_data:
-                title_data[word] += 1
+            if word.lower() in title_data:
+                title_data[word.lower()] += 1
             else:
-                title_data[word] = 1
+                title_data[word.lower()] = 1
 
-    # Sort largest to smallest
-    sorted_title_data = {}
-
-    for key, value in sorted(title_data.items(), key=lambda kv: kv[1], reverse=True):
-        if value >= 8:
-            sorted_title_data[key] = value
-
-    return sorted_title_data, title_data
+    return title_data
     
 def main():
 
-    # Get the titles data of the items sold, one sorted one not sorted  
-    parse_return = parse_ebay()
-    sorted_title_data = parse_return[0]
-    title_data = parse_return[1]
+    # Get the titles data of the items sold 
+    title_data = parse_ebay()
 
     # Split the words (keys) and the counts (values) into two lists
-    words_in_title = list(sorted_title_data.keys())
-    count_of_words = list(sorted_title_data.values())
+    words_in_title = list(title_data.keys())
+    count_of_words = list(title_data.values())
 
     # Test to see if workbook already exists
     try:
@@ -125,6 +116,7 @@ def main():
     # Set value for NEW_MONTH
     ask_if_new_month()
 
+    
     if new_wb:
         # Working in a new workbook
         for i, value in enumerate(words_in_title, 2):
@@ -133,47 +125,39 @@ def main():
         for i, value in enumerate(count_of_words, 2):
             my_sheet.cell(row = i, column = 3).value = value
     else:
-        new_words = []
-        new_counts = []
-        words_added = []
- 
+
+        # Working in an existing workbook
+        
+        # Find how many rows there are
         total_rows = 2
         while my_sheet.cell(row = total_rows, column = 2).value != None:
             total_rows += 1
 
+        # Procedure that depends on new month
         if NEW_MONTH:
             start_row = total_rows
         else:
             start_row = my_sheet.cell(row = 2, column = 6).value
 
-        
-        # If the workbook already exists
-        for counter in range(len(words_in_title)):
-            
-            for i in range(start_row, total_rows):
-                
-                # Not empty cells --> add values 
-                if my_sheet.cell(row = i, column = 2).value == words_in_title[counter]:
+        # Grab the existing words and counts in the sheet
+        existing_data = {}
+        for i in range(start_row, total_rows):
+            existing_data[my_sheet.cell(row = i, column = 2).value.lower()] = my_sheet.cell(row = i, column = 3).value
 
-                    # Update title count   
-                    my_sheet.cell(row = i, column = 3).value += count_of_words[counter]
-                    words_added.append(words_in_title[counter].lower())
-                    continue
-          
+        # Update the counts
         for i in range(len(words_in_title)):
-            if words_in_title[i].lower() not in words_added:
-                new_words.append(words_in_title[i])
-                new_counts.append(title_data[words_in_title[i]])
+            if words_in_title[i] in existing_data:
+                count_of_words[i] += existing_data[words_in_title[i]]
+                
+        # Overwrite the data
+        row_number = start_row
+        for i in range(start_row, start_row + len(words_in_title)):
+            if(count_of_words[i - start_row] >= 6):
+                my_sheet.cell(row = row_number, column = 2).value = words_in_title[i - start_row].capitalize()
+                my_sheet.cell(row = row_number, column = 3).value = count_of_words[i - start_row]
+                row_number += 1
 
-        if new_words:
-            index = 0
-            for i in range(total_rows, total_rows + len(new_words)):
-                # Empty cells --> fill them in
-                if my_sheet.cell(row = i, column = 2).value == None:
-                    my_sheet.cell(row = i, column = 2).value = new_words[index]
-                    my_sheet.cell(row = i, column = 3).value = new_counts[index]
-                index += 1
-
+       
         if NEW_MONTH:
             my_sheet.cell(row = 2, column = 6).value = total_rows
 
