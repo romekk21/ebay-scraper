caf6db1e93d752ef8603192d9cf4206a2464732b
romekk21 <romek.kozdrowicz@gmail.com>
2022-08-11 11:43:12 -0400

    Code cleanup and data pull

--- ebay_scraper_code.py
+++ ebay_scraper_code.py
@@ -4,12 +4,6 @@ from bs4 import BeautifulSoup
 import sys
 import tkinter as tk
 
-# Setup user input dialogue window
-window = tk.Tk()
-window.title('eBay Scraper')
-window.geometry('400x200')
-NEW_MONTH = False
-
 def handle_yes():
     window.destroy()
     global NEW_MONTH
@@ -34,16 +28,16 @@ def ask_if_new_month():
 
 def parse_ebay():
 
-    # Used in order to get rid of emojis 
+    # Used in order to get rid of emojis in title
     non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
-    # Used in order to get past page 5
+    # Used in order to get past page 5 - Otherwise eBay flags for automation
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36'}
 
     # Title storage
     titles = []
 
-    # Loop 10 pages
+    # Loop 10 pages (600 listings)
     for page_num in range(1, 11):
         link = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=vintage+t+shirt&_sacat=1059&rt=nc&LH_Sold=1&LH_Complete=1&_pgn='+str(page_num)
         response = get(link, headers=headers, timeout = 2)
@@ -57,17 +51,23 @@ def parse_ebay():
         for post in posts:
             titles.append(post.find('h3', class_='s-item__title').text.translate(non_bmp_map))
 
+    return data_cleanup(titles)
+
+
+def data_cleanup(titles):
+
     title_data = dict()
 
-    # Clean the data
-    stopwords = ["i","ebay","shop","women","for", "/", "to", "~","a", "men’s", "men's", "&", "and","tshirt","mens", "men", "tee", "vintage", "vtg", "on", "of", "the", "t-shirt", "shirt", "shirt,", "sz", "size", "single", "-", "all", "in", "t"]
+    # Words we don't care about
+    stopwords = ["my", "is", "no", "i","ebay","shop","women","for", "/", "to", "~","a", "men’s", "men's", "&", "and","tshirt","mens", "men", "tee", "vintage", "vtg", "on", "of", "the", "t-shirt", "shirt", "shirt,", "sz", "size", "single", "-", "all", "in", "t"]
 
-    # For each FULL title 
+    # For each FULL title of a listing
     for title in titles:
 
         # Split into a list of the words that make up the title
         words_in_title = title.split()
         
+        # For each word in the title of one listing 
         for word in words_in_title:
 
             # If a word in the title is found in stopwords, remove it from the list
@@ -93,74 +93,76 @@ def parse_ebay():
 
     return title_data
     
-def main():
+# Setup user input dialogue window
+window = tk.Tk()
+window.title('eBay Scraper')
+window.geometry('400x200')
+NEW_MONTH = False
 
-    # Get the titles data of the items sold 
-    title_data = parse_ebay()
+# Get the titles data of the items sold 
+title_data = parse_ebay()
 
-    # Split the words (keys) and the counts (values) into two lists
-    words_in_title = list(title_data.keys())
-    count_of_words = list(title_data.values())
+# Split the words (keys) and the counts (values) into two lists
+words_in_title = list(title_data.keys())
+count_of_words = list(title_data.values())
 
-    # Test to see if workbook already exists
-    try:
-        my_wb = openpyxl.load_workbook(filename="ebay_scraper.xlsx")
-        new_wb = False
-    except:
-        my_wb = openpyxl.Workbook()
-        new_wb = True
+# Test to see if workbook already exists
+try:
+    my_wb = openpyxl.load_workbook(filename="ebay_scraper.xlsx")
+    new_wb = False
+except:
+    my_wb = openpyxl.Workbook()
+    new_wb = True
 
-    # Go to first sheet
-    my_sheet = my_wb.worksheets[0]
+# Go to first sheet
+my_sheet = my_wb.worksheets[0]
 
-    # Set value for NEW_MONTH
-    ask_if_new_month()
+# Set value for NEW_MONTH
+ask_if_new_month()
+
+
+if new_wb:
+    # Working in a new workbook
+    for i, value in enumerate(words_in_title, 2):
+        my_sheet.cell(row = i, column = 2).value = value
+
+    for i, value in enumerate(count_of_words, 2):
+        my_sheet.cell(row = i, column = 3).value = value
+else:
+
+    # Working in an existing workbook
+    
+    # Find how many rows there are
+    total_rows = 2
+    while my_sheet.cell(row = total_rows, column = 2).value != None:
+        total_rows += 1
+
+    # Procedure that depends on new month
+    if NEW_MONTH:
+        start_row = total_rows
+    else:
+        start_row = my_sheet.cell(row = 2, column = 6).value
+
+    # Grab the existing words and counts in the sheet
+    existing_data = {}
+    for i in range(start_row, total_rows):
+        existing_data[my_sheet.cell(row = i, column = 2).value.lower()] = my_sheet.cell(row = i, column = 3).value
+
+    # Update the counts
+    for i in range(len(words_in_title)):
+        if words_in_title[i] in existing_data:
+            count_of_words[i] += existing_data[words_in_title[i]]
+            
+    # Overwrite the data
+    row_number = start_row
+    for i in range(start_row, start_row + len(words_in_title)):
+        if(count_of_words[i - start_row] >= 6):
+            my_sheet.cell(row = row_number, column = 2).value = words_in_title[i - start_row].capitalize()
+            my_sheet.cell(row = row_number, column = 3).value = count_of_words[i - start_row]
+            row_number += 1
 
     
-    if new_wb:
-        # Working in a new workbook
-        for i, value in enumerate(words_in_title, 2):
-            my_sheet.cell(row = i, column = 2).value = value
+    if NEW_MONTH:
+        my_sheet.cell(row = 2, column = 6).value = total_rows
 
-        for i, value in enumerate(count_of_words, 2):
-            my_sheet.cell(row = i, column = 3).value = value
-    else:
-
-        # Working in an existing workbook
-        
-        # Find how many rows there are
-        total_rows = 2
-        while my_sheet.cell(row = total_rows, column = 2).value != None:
-            total_rows += 1
-
-        # Procedure that depends on new month
-        if NEW_MONTH:
-            start_row = total_rows
-        else:
-            start_row = my_sheet.cell(row = 2, column = 6).value
-
-        # Grab the existing words and counts in the sheet
-        existing_data = {}
-        for i in range(start_row, total_rows):
-            existing_data[my_sheet.cell(row = i, column = 2).value.lower()] = my_sheet.cell(row = i, column = 3).value
-
-        # Update the counts
-        for i in range(len(words_in_title)):
-            if words_in_title[i] in existing_data:
-                count_of_words[i] += existing_data[words_in_title[i]]
-                
-        # Overwrite the data
-        row_number = start_row
-        for i in range(start_row, start_row + len(words_in_title)):
-            if(count_of_words[i - start_row] >= 6):
-                my_sheet.cell(row = row_number, column = 2).value = words_in_title[i - start_row].capitalize()
-                my_sheet.cell(row = row_number, column = 3).value = count_of_words[i - start_row]
-                row_number += 1
-
-       
-        if NEW_MONTH:
-            my_sheet.cell(row = 2, column = 6).value = total_rows
-
-    my_wb.save("ebay_scraper.xlsx")
-
-main()
+my_wb.save("ebay_scraper.xlsx")
